48f30163e52f4ae36c94b32aec984ec25f40bef3
18millo <levinjenga097@gmail.com>
2025-07-04 12:04:59 +0300

    first commit

---
+++ variables/variables.js
@@ -0,0 +1,45 @@
+// // A Variable is a container that stores a value. It behaves as if it were the value it contains
+// // Declaration let x;
+// // Assignment x = 100;
+//       //Numbers
+// let x;
+// let y;
+
+// x = 100;
+
+// console.log(x)
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// console.log(`You are ${age} years old`)
+// console.log(`The price is: &${price}`)
+// console.log(`Your gpa is: ${gpa}`)
+// console.log(typeof age)
+//       //Strings
+// let firstname = 'Millo';
+// let favouritefood = 'pizza';
+// let email = 'bro@gmail.com';
+// console.log(typeof firstname);
+// console.log(`My name is ${firstname}`);
+// console.log(`your email is ${email}`)
+
+//      //Booleans
+
+// let online = true;
+
+// console.log(`Bro is online: ${online}`)
+
+// let forsale = false;
+
+// let isStudent = true;
+
+
+let fullname = 'Millo Gegs'
+let myAge = 25;
+let student = false;
+
+document.getElementById('p1').textContent = `My full name is ${fullname}`
+document.getElementById('p2').textContent = `I am ${myAge} old`;
+document.getElementById('p3').textContent = `Student = ${student}`;
